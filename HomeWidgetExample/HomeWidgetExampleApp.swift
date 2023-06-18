6ac9e5e719c39f7c18f47756d823f95ffdbf24aa
ios-min <kt.min@bucketplace.net>
2023-06-18 22:17:57 +0900

    Initial Commit

---
+++ HomeWidgetExample/HomeWidgetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWidgetExampleApp.swift
+//  HomeWidgetExample
+//
+//  Created by Min Min on 2023/06/18.
+//
+
+import SwiftUI
+
+@main
+struct HomeWidgetExampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
