576653a5a9d402f586a0d53aeb406f480f2064a1
ios-min <kt.min@bucketplace.net>
2023-06-23 03:50:14 +0900

    [Chore] 테스트용 API Client로 위젯 업데이트 예제 구성

--- HomeWidget/HomeWidget.swift
+++ HomeWidget/HomeWidget.swift
@@ -9,6 +9,44 @@ import WidgetKit
 import SwiftUI
 import Intents
 
+// https://api.sampleapis.com/coffee/hot
+/// Sample Response
+typealias CoffeeResponse = [Coffee]
+
+struct Coffee: Decodable, Equatable {
+  let title: String
+  let description: String
+}
+
+struct MyActivity: Decodable {
+  let activity: String
+}
+
+/// 위젯에 사용할 Sample API Client
+final class APIClient {
+  static var shared = APIClient()
+
+  private func requestData<Model: Decodable>(urlRequest: URLRequest) async throws -> Model {
+    let (data, _) = try await URLSession.shared.data(for: urlRequest)
+    let jsonData = try JSONDecoder().decode(Model.self, from: data)
+    return jsonData
+  }
+
+  func getCoffees() async throws -> CoffeeResponse {
+    let url = URL(string: "https://api.sampleapis.com/coffee/hot")!
+    var urlRequest = URLRequest(url: url)
+    urlRequest.httpMethod = "GET"
+    return try await requestData(urlRequest: urlRequest)
+  }
+
+  func getMyActiity() async throws -> MyActivity {
+    let url = URL(string: "https://www.boredapi.com/api/activity")!
+    var urlRequest = URLRequest(url: url)
+    urlRequest.httpMethod = "GET"
+    return try await requestData(urlRequest: urlRequest)
+  }
+}
+
 struct Provider: IntentTimelineProvider {
   func placeholder(in context: Context) -> SimpleEntry {
     // 위젯 default 화면 지정
@@ -20,9 +58,25 @@ struct Provider: IntentTimelineProvider {
     in context: Context,
     completion: @escaping (SimpleEntry) -> ()
   ) {
-    // 위젯 추가 화면, 앱의 특정 시점에서 위젯을 업데이트 하고자 할때
-    let entry = SimpleEntry(date: Date(), configuration: configuration)
-    completion(entry)
+    Task {
+      do {
+        let myActivity = try await APIClient.shared.getMyActiity()
+        let entry = SimpleEntry(
+          date: Date(),
+          configuration: configuration,
+          myActivity: myActivity
+        )
+        completion(entry)
+      } catch {
+        let entry = SimpleEntry(
+          date: Date(),
+          configuration: configuration,
+          myActivity: nil
+        )
+        completion(entry)
+      }
+      // 위젯 추가 화면, 앱의 특정 시점에서 위젯을 업데이트 하고자 할때
+    }
   }
   
   func getTimeline(
@@ -30,10 +84,11 @@ struct Provider: IntentTimelineProvider {
     in context: Context,
     completion: @escaping (Timeline<Entry>) -> ()
   ) {
-    var entries: [SimpleEntry] = []
-    
     // Generate a timeline consisting of five entries an hour apart, starting from the current date.
     // 업데이트 할 시점을 정의해서 주기적으로 홈화면의 위젯을 업데이트하고자 할때 사용
+    /*
+    var entries: [SimpleEntry] = []
+
     let currentDate = Date()
     for hourOffset in 0 ..< 5 {
       let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
@@ -42,20 +97,48 @@ struct Provider: IntentTimelineProvider {
     }
     
     let timeline = Timeline(entries: entries, policy: .atEnd) // .atEnd, .never, .after
-    completion(timeline)
+    */
+    Task {
+      do {
+        let myActivity = try await APIClient.shared.getMyActiity()
+        let entry = SimpleEntry(
+          date: Date(),
+          configuration: configuration,
+          myActivity: myActivity
+        )
+        let nextDate = Calendar.current.date(bySetting: .minute, value: 3, of: Date())!
+        let timeline = Timeline(entries: [entry], policy: .after(nextDate))
+        completion(timeline)
+      } catch {
+        let entry = SimpleEntry(
+          date: Date(),
+          configuration: configuration,
+          myActivity: nil
+        )
+        let timeline = Timeline(entries: [entry], policy: .atEnd)
+        completion(timeline)
+      }
+    }
   }
 }
 
 struct SimpleEntry: TimelineEntry {
   let date: Date
   let configuration: ConfigurationIntent
+  var myActivity: MyActivity?
 }
 
 struct HomeWidgetEntryView : View {
   var entry: Provider.Entry
   
   var body: some View {
-    Text(entry.date, style: .time)
+    VStack {
+      Text(entry.date, style: .time)
+      if let response = entry.myActivity {
+        Text(response.activity)
+          .font(.caption)
+      }
+    }
   }
 }
 
