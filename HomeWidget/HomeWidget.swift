35be873b5d3987767556477289ee09976c471aee
ios-min <kt.min@bucketplace.net>
2023-06-23 03:17:29 +0900

    [Chore] Widget Sample Description

--- HomeWidget/HomeWidget.swift
+++ HomeWidget/HomeWidget.swift
@@ -11,18 +11,29 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
   func placeholder(in context: Context) -> SimpleEntry {
+    // 위젯 default 화면 지정
     SimpleEntry(date: Date(), configuration: ConfigurationIntent())
   }
   
-  func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+  func getSnapshot(
+    for configuration: ConfigurationIntent,
+    in context: Context,
+    completion: @escaping (SimpleEntry) -> ()
+  ) {
+    // 위젯 추가 화면, 앱의 특정 시점에서 위젯을 업데이트 하고자 할때
     let entry = SimpleEntry(date: Date(), configuration: configuration)
     completion(entry)
   }
   
-  func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+  func getTimeline(
+    for configuration: ConfigurationIntent,
+    in context: Context,
+    completion: @escaping (Timeline<Entry>) -> ()
+  ) {
     var entries: [SimpleEntry] = []
     
     // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+    // 업데이트 할 시점을 정의해서 주기적으로 홈화면의 위젯을 업데이트하고자 할때 사용
     let currentDate = Date()
     for hourOffset in 0 ..< 5 {
       let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
@@ -30,7 +41,7 @@ struct Provider: IntentTimelineProvider {
       entries.append(entry)
     }
     
-    let timeline = Timeline(entries: entries, policy: .atEnd)
+    let timeline = Timeline(entries: entries, policy: .atEnd) // .atEnd, .never, .after
     completion(timeline)
   }
 }
@@ -52,17 +63,20 @@ struct HomeWidget: Widget {
   let kind: String = "HomeWidget"
   
   var body: some WidgetConfiguration {
-    IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-      HomeWidgetEntryView(entry: entry)
-    }
-    .configurationDisplayName("My Widget")
-    .description("This is an example widget.")
+    IntentConfiguration(
+        kind: kind, // widget identifier
+        intent: ConfigurationIntent.self, // user configuration
+        provider: Provider(), // placeholder, snapshot, timeline 설정 등
+        content: HomeWidgetEntryView.init // 실제 위젯에 표출될 View
+    )
+    .configurationDisplayName("My Widget") // 위젯 추가화면 타이틀
+    .description("This is an example widget.") // 위젯 추가화면 description
   }
 }
 
 struct HomeWidget_Previews: PreviewProvider {
   static var previews: some View {
     HomeWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-      .previewContext(WidgetPreviewContext(family: .systemSmall))
+      .previewContext(WidgetPreviewContext(family: .systemLarge))
   }
 }
--- HomeWidget/HomeWidgetBundle.swift
+++ HomeWidget/HomeWidgetBundle.swift
@@ -8,6 +8,8 @@
 import WidgetKit
 import SwiftUI
 
+// 생성한 위젯의 메인
+// 일반 홈 위젯만 사용한다면, Widget만 준수하면 되나, LiveActivity까지 사용하고 싶다면 단일 위젯 내에 다수의 Widget을 사용할 수 있게 하는 Bundle protocol을 채택하여 사용
 @main
 struct HomeWidgetBundle: WidgetBundle {
   var body: some Widget {
@@ -15,3 +17,4 @@ struct HomeWidgetBundle: WidgetBundle {
     HomeWidgetLiveActivity()
   }
 }
+
